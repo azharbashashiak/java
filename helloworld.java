aca2425ecd82a9a27fe2f4c5a1f8eeeae8d95d20
EC2 Default User <ec2-user@ip-172-31-41-39.ap-south-1.compute.internal>
2021-07-27 16:41:44 +0000

    added ew lineso ceck the webhook

--- helloworld.java
+++ helloworld.java
@@ -3,6 +3,7 @@ public class helloworld{
  public static void main(String args[]){
  System.out.println("Hello world, to the new devops");
  System.out.println("welcome to the world o jenkins");
+ System.out.println("Hers a new commit to test webhooks");
  }
 
 }
