6939169b1a9ceab5e3b2ff006a008d976de7096c
EC2 Default User <ec2-user@ip-172-31-41-39.ap-south-1.compute.internal>
2021-07-27 16:39:10 +0000

    added ew lines

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,7 @@ public class helloworld{
 
  public static void main(String args[]){
  System.out.println("Hello world, to the new devops");
+ System.out.println("welcome to the world o jenkins");
  }
 
 }
