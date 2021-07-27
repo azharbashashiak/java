b8197a80bfbc0176288e1baec92dd6a3e09ecd08
EC2 Default User <ec2-user@ip-172-31-41-39.ap-south-1.compute.internal>
2021-07-27 17:05:26 +0000

    added console

--- helloworld.java
+++ helloworld.java
@@ -4,7 +4,7 @@ public class helloworld{
  System.out.println("Hello world, to the new devops");
  System.out.println("welcome to the world o jenkins");
  System.out.println("Hers a new commit to test webhooks");
- System.out.pintln("welcome to webhooks");
+ System.out.println("welcome to webhooks");
  System.out.println("hello to scm webhook");
  }
 
